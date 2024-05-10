1129c68c5a632fb5079879b99dd8b642e904315a
lawrence <lawcubegsm@gmail.com>
2024-05-10 20:36:07 +0100

    Add slider component

---
+++ src/components/slider.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React, {useRef, useState} from 'react'
+import { AiOutlineClose } from "react-icons/ai";
+
+
+export default function Drawer({ children,isOpen = false, setIsOpen}: { children: React.ReactNode, isOpen : boolean, setIsOpen : any}) {
+
+
+      
+    return (
+        <>
+            {isOpen && <div className='fixed flex justify-end top-0 right-0 bottom-0 left-0  w-screen h-screen bg-light-blue/30 z-30 drop-shadow-2xl ' >
+                <div className='h-full w-[20rem] bg-white p-3'>
+                    <div className='flex justify-end'>
+                        <button onClick={()=>setIsOpen(false)} className='p-2 rounded-md shadow-sm ring-2 ring-light-blue hover:bg-light-blue/10'><AiOutlineClose /></button>
+                    </div>
+                    {children}
+                </div>
+     
+
+            </div >}
+        </>
+  )
+}
