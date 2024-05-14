1d6c86137c3a11d3b81764193a325ac731bd7107
lawrence <lawcubegsm@gmail.com>
2024-05-14 23:07:45 +0100

    Add cards layout

---
+++ src/components/card/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CardLayout = ({children}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="relative flex flex-col flex-initial flex-shrink  shadow-sm border-[0.1px] max-w-[22rem] h-fit min-w-[12rem]
+    rounded-2xl border-none hover:scale-105 transition-all duration-700">
+          {children}
+      </div>
+  )
+}
+
+export default CardLayout
