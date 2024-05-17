7e0c757a80d70c61ef37794823ce04a6123b9ef3
lawrence <lawcubegsm@gmail.com>
2024-05-17 20:51:25 +0100

    Add form, map section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,58 @@ export default function Home() {
       </section>
       
 
+      <section className="md:mx-4 text-xl font-light px-4 md:px-10 h-fit mt-24  mb-20">
+          <h2 className={` text-center flex flex-col gap-3 text-2xl font-medium capitalize after:h-[2px] after:w-12 after:self-center 
+              after:bg-red-600 mb-10 `}>
+                Contact us
+        </h2>
+            
+
+        <div className="flex flex-col lg:flex-row items-center justify-between  h-fit gap-24 lg:gap-4 w-full ">
+          <div className="w-full lg:w-[45%] h-[35rem] max-h-fit ">
+            <h3 className="font-medium">Send us message</h3>
+            <form className="flex flex-col justify-between w-full  ">
+              <div className="flex  justify-between w-full">
+                <div className="w-[45%] mt-4">
+                                <Input type = 'first name' placeholder='First Name' name='first_name' required = {true} id='first_name' label = 'First Name'/>
+
+                </div>
+                <div className="w-[45%] mt-4">
+                                <Input type = 'last name' placeholder='last name' name='last_name' required = {true} id='last_name' label = 'Last Name'/>
+
+                </div>
+              </div>
+              
+              <div className="mt-4"> 
+                      <Input name='email' label='email address' type='email' placeholder='Email Address' required />
+
+              </div>
+              
+              <div className="mt-4"> 
+
+                  <label htmlFor="last_name" className="block mb-2  text-base text-gray-900 capitalize w-full ">Message</label>
+
+                  <textarea className=" bg-white border text-gray-900 text-sm rounded-lg border-dark-blue focus:shadow-mid-blue block w-full p-2.5
+          shadow-sm outline-0 invalid:border-red-00 valid:ring-dark-blue" placeholder="your message here" rows={10}  >
+
+                  </textarea>
+              </div>
+              <div className="w-full flex justify-center justify-self-end  mt-6">
+                <input type="submit " value={'send'} className="text-white bg-dark-blue text-center py-2 w-full rounded-lg "/>
+              </div>
+            </form>
+          </div>
+          <div className="w-full lg:w-[45%] max-h-fit  ">
+            <div className="flex gap-2 mb-4 items-center font-medium"><SlLocationPin /> MK1 1QT </div>
+            <div className="w-full relative  h-[32rem]">
+              {/* <MapContainer apiKey={process.env.GOOGLE_MAP_KEY || ''} mapId={process.env.GOOGLE_MAP_KEY || ''} /> */}
+         
+            </div>
+           
+          </div>
+        </div>
+    
+      </section> 
     </main>
   );
 }
