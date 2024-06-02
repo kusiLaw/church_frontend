f60962f32274517f1e282e6f1eab2d4a299fc7a5
lawrence <lawcubegsm@gmail.com>
2024-06-02 09:22:04 +0100

    Fix all links
    - remove wednesday program from weekly activity
    - move weeakly activity into shared events section

--- src/components/social_media.tsx
+++ src/components/social_media.tsx
@@ -9,18 +9,22 @@ import { PiYoutubeLogoBold } from "react-icons/pi";
 const SocialMedia = ({primary = true}) => {
   return (
     <>
-         <Link href={''} className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ': ' bg-inherit' }  `}>
+      <Link href={'https://www.facebook.com/MKINTSDACHURCH'} target='_blank'
+        className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ' : ' bg-inherit'}  `}>
           <FaFacebookF  />
         
         </Link> 
-          <Link href={'https://www.youtube.com/channel/UCLDWCcGi3uU60dtGHnfVDhA'} target='blank' className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ': ' bg-inherit' }  `}>
+      <Link href={'https://www.youtube.com/channel/UCLDWCcGi3uU60dtGHnfVDhA'} target='_blank'
+        className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ' : ' bg-inherit'}  `}>
           <PiYoutubeLogoBold />
         </Link> 
     
-      <Link href={''} className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ': ' bg-inherit' }  `}>
+      <Link href={'https://x.com/KeynesSda'} target='_blank'
+        className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ' : ' bg-inherit'}  `}>
           <FaXTwitter  />
         </Link> 
-        <Link href={''}className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ': ' bg-inherit' }  `}>
+      <Link href={'https://www.tiktok.com/@mkintsdayouth'} target='_blank'
+        className={`inline-flex justify-center items-center p-1 borer rounded-full hover:scale-105 ${primary ? 'bg-mid-blue  shadow-sm hover:shadow-mid-blue/40 ' : ' bg-inherit'}  `}>
           <FaTiktok />
         </Link> 
     
